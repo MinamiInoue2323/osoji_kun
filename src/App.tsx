f8b88768392ec3d24168ae9815ea1d0278bd323e
MinamiInoue2323 <inoue@ics.keio.ac.jp>
2022-09-04 15:39:32 +0900

    add react-router-dom and routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,21 @@
 import "./App.css";
 import TimerPage from "./pages/TimerPage";
-// import PlacePage from "./pages/PlacePage";
-// import ConfigPage from "./pages/ConfigPage";
+import PlacePage from "./pages/PlacePage";
+import ConfigPage from "./pages/ConfigPage";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
-  return <TimerPage />;
+  return (
+    <BrowserRouter>
+      <div>
+        <Routes>
+          <Route path="/" element={<TimerPage />} />
+          <Route path="/place" element={<PlacePage />} />
+          <Route path="/config" element={<ConfigPage />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
 }
 
 export default App;
