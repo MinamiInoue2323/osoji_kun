68588d572bf6d4b2a2cdcd825c9be1b70e8c8186
MinamiInoue2323 <inoue@ics.keio.ac.jp>
2022-09-04 15:25:35 +0900

    remove eslint warnning

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,10 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
 import "./App.css";
 import TimerPage from "./pages/TimerPage";
-import PlacePage from "./pages/PlacePage";
-import ConfigPage from "./pages/ConfigPage";
+// import PlacePage from "./pages/PlacePage";
+// import ConfigPage from "./pages/ConfigPage";
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  return <ConfigPage />;
+  return <TimerPage />;
 }
 
 export default App;
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,9 @@
-import { useState } from "react";
-import reactLogo from "../assets/react.svg";
 import "../App.css";
 import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
-import Button from "@mui/material/Button";
+
 import IconButton from "@mui/material/IconButton";
 import MenuIcon from "@mui/icons-material/Menu";
 
