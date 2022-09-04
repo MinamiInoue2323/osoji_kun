87d8dbc54f5d518b00e23f3afad01362957104c8
MinamiInoue2323 <inoue@ics.keio.ac.jp>
2022-09-04 17:32:40 +0900

    modify react import

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import * as React from "react";
+import React from "react";
 import TimerPage from "./pages/TimerPage";
 import PlacePage from "./pages/PlacePage";
 import ConfigPage from "./pages/ConfigPage";
