4a7846cfe44421ba3cca79d6d5afadde429f8913
MinamiInoue2323 <inoue@ics.keio.ac.jp>
2022-09-06 10:45:03 +0900

    add timepicker

--- src/pages/ConfigPage.tsx
+++ src/pages/ConfigPage.tsx
@@ -1,16 +1,27 @@
 import React from "react";
 import { Button, TextField, Typography } from "@mui/material";
-// import { LocalizationProvider, TimePicker } from "@mui/x-date-pickers";
+import { LocalizationProvider, TimePicker } from "@mui/x-date-pickers";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import { Dayjs } from "dayjs";
 // import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 
 const ConfigPage = () => {
+  const [value, setValue] = React.useState<Dayjs | null>(null);
   return (
     <div>
       <div>
         <Typography variant="h4" component="p" gutterBottom>
           通知時間
         </Typography>
-        <TextField id="standard-basic" placeholder="09:00" variant="standard" />
+        <LocalizationProvider dateAdapter={AdapterDayjs}>
+          <TimePicker
+            value={value}
+            onChange={(newValue) => {
+              setValue(newValue);
+            }}
+            renderInput={(params) => <TextField {...params} />}
+          />
+        </LocalizationProvider>
       </div>
       <div>
         <Button variant="contained">設定を保存</Button>
