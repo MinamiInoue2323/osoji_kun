9a697e0349e74c040e2941718487cce193e4ae56
MinamiInoue2323 <inoue@ics.keio.ac.jp>
2023-05-19 17:29:28 +0900

    [modify]recoil-persistで状態を永続化

--- src/states/finishPlaceListState.ts
+++ src/states/finishPlaceListState.ts
@@ -1,9 +1,13 @@
 import { atom, useRecoilState } from "recoil";
+import { recoilPersist } from "recoil-persist";
 import { Place } from "../hooks/usePlace";
+
+const { persistAtom } = recoilPersist();
 
 const finishPlaceListState = atom({
   key: "finishplacelist",
   default: [] as Place[],
+  effects_UNSTABLE: [persistAtom],
 });
 
 export const useFinishPlaceList = () => {
